38aaedf8d2ea2526abf94b140fae2f5ce3e3bd12
mousedoc <sangyun3631@gmail.com>
2018-04-25 16:59:16 +0900

    Create project

---
+++ unity-tts/Assets/Scripts/Dialog/DialogControllerConfirm.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.UI;
+
+public class DialogControllerConfirm : DialogControllerWithData<DialogDataConfirm>
+{
+    #region Inspector set
+
+    public Text TitleText;
+    public Text DescriptionText;
+    public Text ConfirmText;
+
+    #endregion Inspector set
+
+    public override void Build(DialogData data)
+    {
+        base.Build(data);
+
+        SetText();
+    }
+
+    private void SetText()
+    {
+        TitleText.text = Data.TitleKey.ToLocalize();
+        DescriptionText.text = Data.DescriptionKey.ToLocalize();
+        ConfirmText.text = Data.ConfirmTextKey.ToLocalize();
+    }
+
+    public void ConfirmButtonClick()
+    {
+        if (Data.ConfirmCallback != null)
+            Data.ConfirmCallback();
+    }
+}
