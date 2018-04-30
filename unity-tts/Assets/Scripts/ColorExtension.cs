e5da255edc3d2a9057a54ccb928f69ce07472208
MouseDoc <sangyun3631@gmail.com>
2018-04-30 18:41:53 +0900

    Add color extension

---
+++ unity-tts/Assets/Scripts/ColorExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class ColorExtension
+{
+    public static Color ToTranslucent(this Color color)
+    {
+        color.a = 0.5f;
+        return color;
+    }
+
+    public static Color ToOpacity(this Color color)
+    {
+        color.a = 1.0f;
+        return color;
+    }
+}
