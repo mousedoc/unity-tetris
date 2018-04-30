a1d465ca6166763f8643012fc75ec324199d3a68
mousedoc <sangyun3631@gmail.com>
2018-04-30 21:39:48 +0900

    add color

--- unity-tts/Assets/Scripts/ColorExtension.cs
+++ unity-tts/Assets/Scripts/ColorExtension.cs
@@ -1,8 +1,59 @@
 ﻿using UnityEngine;
+using System;
 
 public static class ColorExtension
 {
-    public static Color ToTranslucent(this Color color)
+    public static Color GetRandomColor()
+    {
+        var type = EnumExtension.GetRandom<BlockColorType>();
+
+        switch(type)
+        {
+            case BlockColorType.Blue:
+                return "81C7FCFF".ToColorByHex();
+
+            case BlockColorType.Sky:
+                return "9ED5FDFF".ToColorByHex();
+
+            case BlockColorType.Yellow:
+                return "FAE7C6FF".ToColorByHex();
+
+            case BlockColorType.Orange:
+                return "FEC19DFF".ToColorByHex();
+
+            case BlockColorType.Red:
+                return "FC8885FF".ToColorByHex();
+
+            default:
+                return Color.white;
+        }
+    }
+
+    public static Color ToColorByHex(this string hex)
+    {
+        hex = hex.Replace("0x", "");
+        hex = hex.Replace("#", "");
+
+        byte a = 255;
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+        if (hex.Length == 8)
+        {
+            a = byte.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+
+        return new Color(r, g, b, a);
+    }
+
+    public static Color ToTransparent(this Color color)
+    {
+        color.a = 0.0f;
+        return color;
+    }
+
+    public static Color ToHalfTransparent(this Color color)
     {
         color.a = 0.5f;
         return color;
